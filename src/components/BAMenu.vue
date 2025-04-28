d532e618d10221e5ab5c374e28de8e60d8f7d361
gschoebrth <gregoryschoberth@gmail.com>
2025-04-28 18:42:16 -0400

    ba-update

--- src/components/BAMenu.vue
+++ src/components/BAMenu.vue
@@ -1,38 +1,43 @@
 <script setup>
-import { defineEmits } from 'vue'
-import { storeToRefs } from 'pinia'
-import { useGameStore } from '@/stores/game'
-import { useTrackerStore } from '@/stores/tracker'
-import { onClickOutside } from '@vueuse/core'
-import { useTemplateRef } from 'vue'
+import { defineEmits } from "vue";
+import { storeToRefs } from "pinia";
+import { useGameStore } from "@/stores/game";
+import { useTrackerStore } from "@/stores/tracker";
+import { onClickOutside } from "@vueuse/core";
+import { useTemplateRef } from "vue";
 
-const { resetAfterNoInteraction } = storeToRefs(useTrackerStore())
-const { gameType, maxActionDelay } = storeToRefs(useGameStore())
+const { resetAfterNoInteraction } = storeToRefs(useTrackerStore());
+const { gameType, maxActionDelay } = storeToRefs(useGameStore());
 
-const emit = defineEmits(['ba-close'])
-const target = useTemplateRef('target')
+const emit = defineEmits(["ba-close"]);
+const target = useTemplateRef("target");
 
 const handleClick = () => {
-  emit('ba-close')
-}
+  emit("ba-close");
+};
 
-onClickOutside(target, () => handleClick)
+onClickOutside(target, () => handleClick);
 </script>
 
 <template>
+  <div class="overlay"></div>
   <div class="ba-wrapper" ref="target">
     <div class="input-wrapper">
       <label for="game-type">Choose game type:</label>
       <select name="game-type" v-model="gameType">
         <option value="soccer" selected>Soccer</option>
-        <option value="football" disabled>Football</option>
+        <option value="football">Football</option>
         <option value="baseball" disabled>Baseball</option>
         <option value="hockey" disabled>Hockey</option>
       </select>
     </div>
     <div class="input-wrapper">
       <label for="timeout-delay">Reset Timeout Delay (seconds): </label>
-      <input name="timeout-delay" type="number" v-model="resetAfterNoInteraction" />
+      <input
+        name="timeout-delay"
+        type="number"
+        v-model="resetAfterNoInteraction"
+      />
     </div>
     <div class="input-wrapper">
       <label for="user-delay">Range Max Before Player Action (seconds):</label>
@@ -58,13 +63,9 @@ onClickOutside(target, () => handleClick)
   flex-direction: column;
   gap: 24px;
 
-  // &:after {
-  //   content: '';
-  //   position: fixed;
-  //   inset: 0 0 0 0;
-  //   z-index: 20;
-  //   background-color: rgba(0, 0, 0, 0.5);
-  // }
+  &:after {
+    content: "";
+  }
 }
 
 .input-wrapper {
@@ -75,6 +76,17 @@ onClickOutside(target, () => handleClick)
   label {
     font-weight: bold;
   }
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100dvw;
+  height: 100dvh;
+  // transform: translate(-50%, -50%);
+  z-index: 9999;
+  background-color: rgba(0, 0, 0, 0.5);
 }
 
 .btn-close {
