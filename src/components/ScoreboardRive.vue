9676a6e0081634207b71f61898a52e37d550fc41
gschoebrth <gregoryschoberth@gmail.com>
2025-04-28 19:50:35 -0400

    updated scoring

--- src/components/ScoreboardRive.vue
+++ src/components/ScoreboardRive.vue
@@ -1,14 +1,14 @@
 <script lang="js" setup>
-import { onMounted, ref, watch } from 'vue'
-import { EventType, Layout, Rive } from '@rive-app/canvas'
-import { useGameStore } from '@/stores/game'
-import { storeToRefs } from 'pinia'
+import { onMounted, ref, watch } from "vue";
+import { EventType, Layout, Rive } from "@rive-app/canvas";
+import { useGameStore } from "@/stores/game";
+import { storeToRefs } from "pinia";
 
-const game = useGameStore()
-const { currAttempt, showScore } = storeToRefs(game)
+const game = useGameStore();
+const { currAttempt, showScore } = storeToRefs(game);
 
-const canvas = ref([])
-let inputs = null
+const canvas = ref([]);
+let inputs = null;
 
 // Watch for events coming from Rive
 // Move this to it's own module on refactor
@@ -21,19 +21,19 @@ let inputs = null
 // to a dedicated script for each animation / functionality
 
 function onRiveEvent(event) {
-  const eventData = event.data
+  const eventData = event.data;
 
-  console.log(eventData.name)
+  console.log(eventData.name);
 
-  if (eventData.name === 'resetEvent') {
-    game.resetGame()
-  } else if (eventData.name === 'nextEvent') {
+  if (eventData.name === "resetEvent") {
+    game.resetGame();
+  } else if (eventData.name === "nextEvent") {
     // const attemptTrigger = inputs.find((i) => i.name === 'attempt')
     // // attemptTrigger.value = currAttempt.value
     // attemptTrigger.value = game.currAttempt
-    game.userVictory = false
-    game.startNewGame()
-    game.showScore = false
+    game.userVictory = false;
+    game.startNewGame();
+    game.showScore = false;
   }
 }
 
@@ -42,48 +42,52 @@ onMounted(() => {
     src: `rive/scoreboard.riv`,
     canvas: canvas.value,
     autoplay: true,
-    stateMachines: 'default',
+    stateMachines: "default",
 
     layout: new Layout({}),
     onLoad: () => {
-      r.resizeDrawingSurfaceToCanvas()
-      const i = r.stateMachineInputs('default')
-      inputs = i
+      r.resizeDrawingSurfaceToCanvas();
+      const i = r.stateMachineInputs("default");
+      inputs = i;
 
-      const attemptTrigger = inputs.find((i) => i.name === 'attempt')
-      const displayTrigger = inputs.find((i) => i.name === 'display')
+      const scoreTrigger = inputs.find((i) => i.name === "score");
+      const attemptTrigger = inputs.find((i) => i.name === "attempt");
+      const displayTrigger = inputs.find((i) => i.name === "display");
 
-      attemptTrigger.value = game.currAttempt
-      displayTrigger.value = false
+      scoreTrigger.value = game.currScore;
+      attemptTrigger.value = game.currAttempt;
+      displayTrigger.value = false;
     },
-  })
+  });
 
-  r.on(EventType.RiveEvent, onRiveEvent)
-})
+  r.on(EventType.RiveEvent, onRiveEvent);
+});
 
 watch(
   () => showScore.value,
   (newVal) => {
-    const attemptTrigger = inputs.find((i) => i.name === 'attempt')
-    const displayTrigger = inputs.find((i) => i.name === 'display')
+    const scoreTrigger = inputs.find((i) => i.name === "score");
+    const attemptTrigger = inputs.find((i) => i.name === "attempt");
+    const displayTrigger = inputs.find((i) => i.name === "display");
     if (newVal) {
-      attemptTrigger.value = game.currAttempt
-      displayTrigger.value = true
+      attemptTrigger.value = game.currAttempt;
+      scoreTrigger.value = game.currScore;
+      displayTrigger.value = true;
     } else {
-      displayTrigger.value = false
+      displayTrigger.value = false;
     }
-  },
-)
+  }
+);
 
 game.$onAction(({ name }) => {
-  if (name === 'resetGame') {
-    const attemptTrigger = inputs.find((i) => i.name === 'attempt')
-    const displayTrigger = inputs.find((i) => i.name === 'display')
+  if (name === "resetGame") {
+    const attemptTrigger = inputs.find((i) => i.name === "attempt");
+    const displayTrigger = inputs.find((i) => i.name === "display");
 
-    attemptTrigger.value = 0
-    displayTrigger.value = false
+    attemptTrigger.value = 0;
+    displayTrigger.value = false;
   }
-})
+});
 </script>
 
 <template>
--- src/stores/game.js
+++ src/stores/game.js
@@ -1,76 +1,80 @@
-import { ref } from 'vue'
-import { defineStore } from 'pinia'
+import { ref } from "vue";
+import { defineStore } from "pinia";
 
-export const useGameStore = defineStore('game', () => {
-  const gameType = ref('soccer')
+export const useGameStore = defineStore("game", () => {
+  const gameType = ref("soccer");
 
-  const maxAttempts = ref(3)
-  const currAttempt = ref(0)
-  const currScore = ref(0)
+  const maxAttempts = ref(3);
+  const currAttempt = ref(0);
+  const currScore = ref(0);
 
-  const maxActionDelay = ref(5)
-  const actionButtonDelay = ref(5) // The delay before the go button is active
-  const maxActionTime = ref(5) // Allow the user up to five seconds before considering the goal saved
+  const maxActionDelay = ref(5);
+  const actionButtonDelay = ref(5); // The delay before the go button is active
+  const maxActionTime = ref(5); // Allow the user up to five seconds before considering the goal saved
 
-  const initialTime = ref(Date.now()) // Start time of the game after go
-  const actionTime = ref(Date.now()) // Time of player hitting action button
+  const initialTime = ref(Date.now()); // Start time of the game after go
+  const actionTime = ref(Date.now()); // Time of player hitting action button
 
-  const kickCurveOption = ref('easeinout') // Possible way of calculating chance of goal
-  const userVictory = ref(false) // User success or failure state.
+  const kickCurveOption = ref("easeinout"); // Possible way of calculating chance of goal
+  const userVictory = ref(false); // User success or failure state.
 
-  const showIntro = ref(true)
-  const showAction = ref(false)
-  const showOutcome = ref(false)
-  const showScore = ref(false)
+  const showIntro = ref(true);
+  const showAction = ref(false);
+  const showOutcome = ref(false);
+  const showScore = ref(false);
 
   // Don't Reset
-  const actionContent = ref('Kick')
+  const actionContent = ref("Kick");
 
   // Timeouts
-  const enableActionTimeout = ref(null)
+  const enableActionTimeout = ref(null);
 
   // Game Options Don't Reset
   // Move options to own store if there are enough
-  const useRandomizedAction = ref(false)
+  const useRandomizedAction = ref(false);
 
   function updateGameType(newVal) {
-    gameType.value = newVal
+    gameType.value = newVal;
   }
 
   function startNewGame() {
-    currAttempt.value += 1
-    showIntro.value = false
-    const delay = Math.floor(Math.random() * (maxActionDelay.value - 1 + 1)) + 1
-    console.log('Delay:', delay)
+    currAttempt.value += 1;
+    showIntro.value = false;
+    const delay =
+      Math.floor(Math.random() * (maxActionDelay.value - 1 + 1)) + 1;
+    console.log("Delay:", delay);
     enableActionTimeout.value = setTimeout(() => {
-      showAction.value = true
-      initialTime.value = Date.now()
-    }, delay * 1000)
+      showAction.value = true;
+      initialTime.value = Date.now();
+    }, delay * 1000);
   }
 
   function calculateGoal() {
-    showAction.value = false
-    actionTime.value = Date.now()
-    const time = Math.max(Math.abs(actionTime.value - initialTime.value) / 1000, 0)
+    showAction.value = false;
+    actionTime.value = Date.now();
+    const time = Math.max(
+      Math.abs(actionTime.value - initialTime.value) / 1000,
+      0
+    );
 
-    const minChance = evalBezierCurve(time)
+    const minChance = evalBezierCurve(time);
 
-    const randomizedScoreChance = Math.random() * maxActionTime.value
-    userVictory.value = randomizedScoreChance >= minChance
-    showOutcome.value = true
+    const randomizedScoreChance = Math.random() * maxActionTime.value;
+    userVictory.value = randomizedScoreChance >= minChance;
+    showOutcome.value = true;
 
     if (userVictory.value) {
-      currScore.value = Math.min(currScore.value + 1, 3)
+      currScore.value = Math.min(currScore.value + 1, 3);
     }
 
     // Debugging
-    console.log('Initial:', initialTime.value, 'Kick:', actionTime.value)
-    console.log('Actual Time:', time)
-    console.log('Minimum Chance:', minChance)
-    console.log('Random Chance:', randomizedScoreChance)
-    console.log('User Victory Status', userVictory.value)
-    console.log('Current Score', currScore.value)
-    console.log('Current Attempt', currAttempt.value)
+    console.log("Initial:", initialTime.value, "Kick:", actionTime.value);
+    console.log("Actual Time:", time);
+    console.log("Minimum Chance:", minChance);
+    console.log("Random Chance:", randomizedScoreChance);
+    console.log("User Victory Status", userVictory.value);
+    console.log("Current Score", currScore.value);
+    console.log("Current Attempt", currAttempt.value);
   }
 
   // Move to external?
@@ -79,29 +83,29 @@ export const useGameStore = defineStore('game', () => {
       (1 - t) ** 3 * initial +
       3 * (1 - t) ** 2 * t * p1 +
       3 * (1 - t) * t ** 2 * p2 +
-      t ** 3 * final
+      t ** 3 * final;
 
     const curves = {
       easeinout: [0.42, 0.58],
-    }
+    };
 
-    const points = curves[kickCurveOption.value]
-    const valueAtT = bezier(t, points[0], points[1])
+    const points = curves[kickCurveOption.value];
+    const valueAtT = bezier(t, points[0], points[1]);
 
-    return valueAtT
+    return valueAtT;
   }
 
   function resetGame() {
-    showIntro.value = true
-    showAction.value = false
-    showOutcome.value = false
-    showScore.value = false
-    actionTime.value = Date.now()
-    initialTime.value = Date.now()
-    userVictory.value = false
-    currScore.value = 0
-    currAttempt.value = 0
-    clearTimeout(enableActionTimeout)
+    showIntro.value = true;
+    showAction.value = false;
+    showOutcome.value = false;
+    showScore.value = false;
+    actionTime.value = Date.now();
+    initialTime.value = Date.now();
+    userVictory.value = false;
+    currScore.value = 0;
+    currAttempt.value = 0;
+    clearTimeout(enableActionTimeout);
   }
 
   return {
@@ -125,5 +129,5 @@ export const useGameStore = defineStore('game', () => {
     currAttempt,
     currScore,
     showScore,
-  }
-})
+  };
+});
